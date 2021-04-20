fe7ba8a9aa377928121f0490aa096cd61cc5389b
Frank Purcell <fxpurcell@gmail.com>
2021-04-19 19:12:59 -0700

    fix(setup): have py3 and py2.7 junk

---
+++ setup-py-2.7/setup.py
@@ -0,0 +1,48 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'ott.utils',
+    'pillow==5.4.1',
+    'decorator==4.3.2'
+]
+
+extras_require = dict(
+    dev=[],
+)
+
+
+setup(
+    name='ott.icons',
+    version='0.1.1',
+    description='Open Transit Tools - OTT Icons',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    author="Open Transit Tools",
+    author_email="info@opentransittools.org",
+    dependency_links=[
+        'git+https://github.com/OpenTransitTools/utils.git#egg=ott.utils-0.1.0',
+    ],
+    license="Mozilla-derived (http://opentransittools.com)",
+    url='http://opentransittools.com',
+    keywords='ott, osm, maps, icons, transit',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    extras_require=extras_require,
+    tests_require=requires,
+    test_suite="ott.icons.tests",
+    entry_points="""
+        [console_scripts]
+        one_label = ott.icons.route_labels:main
+    """,
+)
